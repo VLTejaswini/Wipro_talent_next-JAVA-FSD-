5031dd7e7e3b9fd9d422fc2b522a8afc3619e5bc
VLTejaswini <vuddantilakshmitejaswini@gmail.com>
2026-07-05 15:53:16 +0530

    Added Language Basics

---
+++ Java fundamentals/Language_basics/que1_example_1.java
@@ -0,0 +1,5 @@
+public class que1_example_1 {
+    public static void main(String[] args) {
+        System.out.println(args[0] + " Technologies " + args[1]);
+    }
+}
---
+++ Java fundamentals/Language_basics/que2_example_1.java
@@ -0,0 +1,5 @@
+public class que2_example_1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome " + args[0]);
+    }
+}
